c804482e6b6300658c2ae7dcd39ce997e2c98333
Luísa Braga <luisa.braga@meliuz.com.br>
2020-09-25 22:31:17 -0400

    alter api endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://tcc-api-elb-1124838076.us-east-2.elb.amazonaws.com';
+const baseURL = 'http://sexism-app-351908187.us-east-1.elb.amazonaws.com/';
 const token = localStorage.getItem('@SexismResearch:token');
 const api = axios.create({
   baseURL,
