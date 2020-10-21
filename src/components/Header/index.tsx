f3265ba716169e036bb20b12231961a88f90b131
LuizAlexandreSilva <alexandrehsilva96@gmail.com>
2020-10-21 12:30:05 -0400

    style: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,21 @@
-import React from  'react';
+import React from 'react';
 
-import { Container } from './styles';
+import { Container, HeaderContent } from './styles';
 import logoImg from '../../assets/logo.png';
 import { useAuth } from '../../context/auth';
 
-const Header:React.FC = () => {
+const Header: React.FC = () => {
   const { signOut } = useAuth();
-  return (<Container>
-    <img width="200px" src={logoImg} alt="SexismResearch" />
-    <a href='/' onClick={signOut}>Logout</a>
-  </Container>)
+  return (
+    <Container>
+      <HeaderContent>
+        <img width="200px" src={logoImg} alt="SexismResearch" />
+        <a href="/" onClick={signOut}>
+          Logout
+        </a>
+      </HeaderContent>
+    </Container>
+  );
 };
 
 export default Header;
